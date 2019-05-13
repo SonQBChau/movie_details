c64d11c4da53b7efae53ff7720a27b701c0bc34e
Son Chau <soncqb@gmail.com>
2019-05-13 17:37:17 -0500

    make widgets

--- lib/widgets/movie_detail_header.dart
+++ lib/widgets/movie_detail_header.dart
@@ -10,58 +10,109 @@ class MovieDetailHeader extends StatelessWidget {
     return Container(
       child: Stack(
         children: <Widget>[
-          Image.asset('images/banner.png'),
+          ArcBannerImage(movie.bannerUrl),
+          MovieInfoRow(movie),
+        ],
+      ),
+    );
+  }
 
-          Row(
+}
+
+
+class ArcBannerImage extends StatelessWidget {
+  ArcBannerImage(this.bannerUrl);
+  final String bannerUrl;
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(bannerUrl);
+  }
+}
+
+class MovieInfoRow extends StatelessWidget {
+  MovieInfoRow(this.movie);
+  final Movie movie;
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 0.0,
+      left: 16.0,
+      right: 16.0,
+      child: Row(
+        children: <Widget>[
+          Poster(movie.posterUrl),
+          SizedBox(width: 10,),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Container(
-                width: 140,
-                height: 180,
-                child: Image.asset('images/poster.png'),
-              ),
-              SizedBox(width: 10,),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              Text(movie.title, style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+              Row(
                 children: <Widget>[
-                  Text('The Secret Life of Pets', style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                  Row(
+                  RatingInformation(movie.rating),
+                  SizedBox(width: 20,),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                      Row(
                         children: <Widget>[
-                          Text('8.0', style: TextStyle(color: Colors.red, fontSize: 20, fontWeight: FontWeight.w300),),
-                          Text('Ratings'),
+                          Icon(Icons.star, color: Colors.red,),
+                          Icon(Icons.star, color: Colors.red,),
+                          Icon(Icons.star, color: Colors.red,),
                         ],
                       ),
-                      SizedBox(width: 20,),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Row(
-                            children: <Widget>[
-                              Icon(Icons.star, color: Colors.red,),
-                              Icon(Icons.star, color: Colors.red,),
-                              Icon(Icons.star, color: Colors.red,),
-                            ],
-                          ),
-                          Text('Grade now'),
-                        ],
-                      ),
+                      Text('Grade now'),
                     ],
                   ),
+                ],
+              ),
 
-                  Row(
-                    children: <Widget>[
-                      Text('Animation'),
-                      Text('Comedy'),
-                    ],
-                  ),
+              Row(
+                children: <Widget>[
+                  Text('Animation'),
+                  Text('Comedy'),
                 ],
               ),
             ],
           ),
         ],
       ),
+    );;
+  }
+
+
+
+}
+
+class RatingInformation extends StatelessWidget {
+  RatingInformation(this.rating);
+  final double rating;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(rating.toString(), style: TextStyle(color: Colors.red, fontSize: 20, fontWeight: FontWeight.w300),),
+        Text('Ratings'),
+      ],
     );
   }
 }
+
+
+class Poster extends StatelessWidget {
+  Poster(this.posterUrl);
+  final String posterUrl;
+  static const POSTER_RATIO = 0.7;
+
+  @override
+  Widget build(BuildContext context) {
+    double height = 180;
+    double width = POSTER_RATIO * height;
+    return Container(
+      width: width,
+      height: height,
+      child: Image.asset(posterUrl),
+    );
+  }
+}
+
