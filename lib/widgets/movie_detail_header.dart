bc5c7d29821b30a735df1361edde9d1014282b98
Son Chau <soncqb@gmail.com>
2019-05-14 10:35:52 -0500

    add clippath

--- lib/widgets/movie_detail_header.dart
+++ lib/widgets/movie_detail_header.dart
@@ -26,7 +26,16 @@ class ArcBannerImage extends StatelessWidget {
   final String bannerUrl;
   @override
   Widget build(BuildContext context) {
-    return Image.asset(bannerUrl);
+    var screenWidth = MediaQuery.of(context).size.width;
+    return ClipPath(
+      clipper: ArcClipper(),
+      child: Image.asset(
+        bannerUrl,
+        width: screenWidth,
+        height: 230,
+        fit: BoxFit.cover,
+      ),
+    );
   }
 }
 
@@ -180,4 +189,33 @@ class Poster extends StatelessWidget {
       child: Image.asset(posterUrl),
     );
   }
+}
+
+/*
+ARC CLIPPER WIDGET
+*/
+class ArcClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0.0, size.height - 30);
+
+    var firstControlPoint = Offset(size.width / 4, size.height);
+    var firstPoint = Offset(size.width / 2, size.height);
+    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
+        firstPoint.dx, firstPoint.dy);
+
+    var secondControlPoint = Offset(size.width - (size.width / 4), size.height);
+    var secondPoint = Offset(size.width, size.height - 30);
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
+        secondPoint.dx, secondPoint.dy);
+
+    path.lineTo(size.width, 0.0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
